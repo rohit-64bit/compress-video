85d0f66b0812d44d5c665ea9f068ff4f6fc97902
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2024-02-11 00:57:06 +0530

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Compress Video
+
+> npm i compress-video
+
+```
+const videoCompress = require('compress-video')
+
+videoCompress()
+
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+const handleVideoCompress = () => {
+
+    console.log("Compressing video...")
+
+    setTimeout(() => {
+        console.log("Video compressed!")
+    }, 3000)
+
+}
+
+module.exports = handleVideoCompress
