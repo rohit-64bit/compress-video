98e2acff4a19dd23fd75d35ceb7325c8e80c97e4
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2024-02-17 04:57:01 +0530

    Update - V2 - Add video compression functionality

--- README.md
+++ README.md
@@ -2,11 +2,35 @@
 
 > npm i compress-video
 
-```
 const videoCompress = require('compress-video')
 
 videoCompress()
 
+```js
+const compressVideo = require('compress-video')
+const path = require('path')
+
+const inputPath = './sample.mp4'
+
+const outputPath = './compressed'
+
+const options = {
+    crf: 32, // better when set between 30-35 after 40 it will ruin the video
+    videoBitrate: '1M', // adjust it according to your video file size
+    audioBitrate: '128k' // keep it as it is only change for special use cases
+}
+
+compressVideo({ inputPath: path.join(__dirname, inputPath), outputPath, options })
+    .then((outputFileName) => {
+        console.log('Compression finished successfully:', outputFileName)
+    })
+    .catch((err) => {
+        console.error('Error during compression:', err)
+    })
 ```
 
-hey folks I have started this package for my weekend project. But i will be working upon it very soon.
+### Message from author
+
+Version 1 - hey folks I have started this package for my weekend project. But i will be working upon it very soon.
+
+Version 1.2.0 - I added the basic utility with the code in this package. In the next update I will try to optimize the cpu utilization and also try to implement hardware acceleration for compatible devices.
--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-const handleVideoCompress = () => {
+const fs = require('fs-extra')
 
-    console.log("Compressing video...")
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
+const ffmpeg = require('fluent-ffmpeg')
 
-    setTimeout(() => {
-        console.log("Video compressed!")
-    }, 3000)
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
+
+// "E:\test\fileUpload\server\uploads\5a9eed.mp4"
+
+const handleVideoCompress = async ({ inputPath, outputPath, options: { crf, videoBitrate, audioBitrate } }) => {
+
+    // wrap this inside a promise
+
+    const promise = new Promise((resolve, reject) => {
+
+        const fileName = inputPath.split('\\').pop()
+
+        const outputFileName = `${fileName.split('.')[0]}_compressed.mp4`
+
+        console.log(inputPath)
+
+        const outputFilePath = `${outputPath}\\${outputFileName}`
+
+        console.log({ crf, videoBitrate, audioBitrate, inputPath, outputPath, outputFileName, outputFilePath })
+
+        fs.ensureDirSync(outputPath)
+
+        // Compression settings
+        const defaultCrf = crf || 23; // Constant Rate Factor (lower value means better quality but larger file size)
+        const defaultVideoBitrate = videoBitrate || '1M'; // Video bitrate in Mbps
+        const defaultAudioBitrate = audioBitrate || '128k'; // Audio bitrate
+
+        // Compress video
+        ffmpeg(inputPath)
+            .videoCodec('libx264')
+            .audioCodec('aac')
+            .outputOptions([
+                '-crf ' + defaultCrf,
+                '-b:v ' + defaultVideoBitrate,
+                '-b:a ' + defaultAudioBitrate
+            ])
+            .on('error', (err) => {
+                console.error('Error during compression:', err);
+                reject({ error: err, message: "Error during compression" })
+            })
+            .on('end', () => {
+                resolve(outputFileName)
+            })
+            .save(outputFilePath);
+
+    })
+
+    return promise
 
 }
 
